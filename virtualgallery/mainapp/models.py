5a75911a9a54454e14ca603d34dd09204ba193ca
egesaray <73111754+egesaray@users.noreply.github.com>
2021-03-24 16:19:22 +0300

    home page

--- virtualgallery/mainapp/models.py
+++ virtualgallery/mainapp/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+
+
+
+
+
+
+
 class ourUser(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)  # relation with django's user # will be implemented later
     username = models.CharField(max_length=255,null=True)
